ccef73dcca38368f2927d440b4d489b64208aff6
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2023-12-24 18:32:24 -0300

    modified:   src/ForestInventory.jl

--- src/ForestInventory.jl
+++ src/ForestInventory.jl
@@ -4,9 +4,6 @@ include(joinpath(@__DIR__, "AAS.jl"))
 include(joinpath(@__DIR__, "Save.jl"))
 include(joinpath(@__DIR__, "ImportData.jl"))
 
-# Importa os módulos auxiliares para cálculos e funções
-import AAS, Save, ImportData
-
 # Exporta a função Inventory possibilitando ser chamada via terminal pelo usuário
 export Inventory
 
