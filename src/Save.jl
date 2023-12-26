bfffb7a3752366569d06b5ca480bbfb49999037d
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2023-12-25 21:20:36 -0300

    modified:   src/Save.jl

--- src/Save.jl
+++ src/Save.jl
@@ -1,9 +1,12 @@
 module Save
 
+# Exporta as funções definidas nesse módulo
 export saveFile
     
+    # Ativa os pacotes usados para a função saveFile funcionar
     using QML, DataFrames, CSV, XLSX 
 
+    # Define a função  saveFile usada para salvar o arquivo de resultado
     function saveFile(Dados, uri)
         
         # Converte a entrada em QString do QML em String do Julia
