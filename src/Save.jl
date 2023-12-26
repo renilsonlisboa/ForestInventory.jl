c7a574bc3c1eaa6767ab680aacd22a8ae4fad31d
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2023-12-25 21:19:09 -0300

    modified:   src/Save.jl

--- src/Save.jl
+++ src/Save.jl
@@ -5,12 +5,14 @@ export saveFile
     using QML, DataFrames, CSV, XLSX 
 
     function saveFile(Dados, uri)
-
+        
+        # Converte a entrada em QString do QML em String do Julia
         uri_s = QString(uri)
 
         # Remover o prefixo "file:///"
         cleaned_path = replace(uri_s, "file:///" => "")
 
+        # Salva a tabela de resultado no PATH selecionado pelo usuário
         XLSX.writetable("$(cleaned_path).xlsx",Resultados=(collect(DataFrames.eachcol(Dados)), DataFrames.names(Dados)), overwrite = true) #Exportar para o Excel
     
     end
