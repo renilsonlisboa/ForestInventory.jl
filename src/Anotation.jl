91e7d0bd28f695454cdf0efc616a7726ae65ba7c
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2023-12-26 23:04:43 -0300

    modified:   src/Anotation.jl

--- src/Anotation.jl
+++ src/Anotation.jl
@@ -4,3 +4,11 @@
 
 #-> Centralizar Valor Inicial do combobox
 #-> Melhorar visual do botão de Iniciar Processamento
+#-> Definir um icone para a janela do APP
+
+####### AAS ############
+
+#-> Programar o botão VIEW para apresentar o DataFrame que foi importado
+#-> Ajustar o botão de aviso quando não foi informado algum dos valores (front + back (qml + julia))
+#-> Inserir uma barra de carregamento abaixo do botão processar que acomapnhe o tempo para processar e concluir o processamento
+#-> Apresentar uma janela com os resultados básicos do inventário 
