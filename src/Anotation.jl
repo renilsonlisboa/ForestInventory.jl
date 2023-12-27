fc887b8b13a408e29eba71bd05b8e97416918c57
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2023-12-26 23:09:24 -0300

    modified:   src/Anotation.jl

--- src/Anotation.jl
+++ src/Anotation.jl
@@ -1,5 +1,19 @@
 ##### Anotações do que Falta ##############
 
+####### GitHub #############
+
+#-> Adicionar uma definição melhor do que se trata o pacote
+#-> Definidir melhor o passo a passo de como utilizar o pacote
+#-> Atualizar badges do Codev
+#-> Definir uma logo oficial do pacote
+
+####### Módulos ############
+
+#-> Definir os melhores nomes para os módulos
+#-> Otimizar as chamadas de pacotes, importando somente oque for utilizar para poupar memória
+#-> Remover arquivos desnecessários do repositório
+
+
 ####### Tela Inicial ############
 
 #-> Centralizar Valor Inicial do combobox
@@ -12,3 +26,5 @@
 #-> Ajustar o botão de aviso quando não foi informado algum dos valores (front + back (qml + julia))
 #-> Inserir uma barra de carregamento abaixo do botão processar que acomapnhe o tempo para processar e concluir o processamento
 #-> Apresentar uma janela com os resultados básicos do inventário 
+#-> Inserir uma barra de carregamento ao lado do botão de importar dados ou então alterar a cor do botão se importado com sucesso
+#-> No aviso de saida apresentar o caminho onde foi salvo o arquivo de saida
