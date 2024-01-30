400cc72f4a03ac08ee1d944d5b2832331adc6943
renilsonlisboa <renilsonlisboajunior@gmail.com>
2024-01-30 10:09:58 -0300

    modified:   src/ForestInventory.jl 	modified:   src/testes/# Auxiliar Estratificada.jl

--- src/ForestInventory.jl
+++ src/ForestInventory.jl
@@ -1,18 +1,18 @@
 module ForestInventory
 
 # Inclui os módulos auxiliares ao escopo principal do projeto
-include(joinpath(@__DIR__, "src/AAS.jl")) # Amostragem Aleatória Simples
-include(joinpath(@__DIR__, "src/ESTRAT.jl")) # Amostragem Estratificada
-include(joinpath(@__DIR__, "src/SIST.jl")) # Amostragem Sistemática
-include(joinpath(@__DIR__, "src/DE.jl")) # Amostragem Dois Estágios
-include(joinpath(@__DIR__, "src/CONGL.jl")) # Amostragem em Conglomerados
-include(joinpath(@__DIR__, "src/MULTI.jl")) # Amostragem co Multíplos Inicios Aleatórios
-include(joinpath(@__DIR__, "src/IND.jl")) # Amostragem Individual
-include(joinpath(@__DIR__, "src/ART.jl")) # Amostragem com Repetição Total
-include(joinpath(@__DIR__, "src/AD.jl")) # Amostragem Dupla
-include(joinpath(@__DIR__, "src/ARP.jl"))  # Amostragem com Repetição Parcial
-include(joinpath(@__DIR__, "src/Save.jl")) # Função para salvar arquivos de resultados
-include(joinpath(@__DIR__, "src/ImportData.jl")) # Função para importar dados .CSV
+include(joinpath(@__DIR__, "AAS.jl")) # Amostragem Aleatória Simples
+include(joinpath(@__DIR__, "ESTRAT.jl")) # Amostragem Estratificada
+include(joinpath(@__DIR__, "SIST.jl")) # Amostragem Sistemática
+include(joinpath(@__DIR__, "DE.jl")) # Amostragem Dois Estágios
+include(joinpath(@__DIR__, "CONGL.jl")) # Amostragem em Conglomerados
+include(joinpath(@__DIR__, "MULTI.jl")) # Amostragem co Multíplos Inicios Aleatórios
+include(joinpath(@__DIR__, "IND.jl")) # Amostragem Individual
+include(joinpath(@__DIR__, "ART.jl")) # Amostragem com Repetição Total
+include(joinpath(@__DIR__, "AD.jl")) # Amostragem Dupla
+include(joinpath(@__DIR__, "ARP.jl"))  # Amostragem com Repetição Parcial
+include(joinpath(@__DIR__, "Save.jl")) # Função para salvar arquivos de resultados
+include(joinpath(@__DIR__, "ImportData.jl")) # Função para importar dados .CSV
 
 # Importar as funções utilizadas provenientes do pacote QML
 import QML: QString, @qmlfunction, loadqml, exec
--- src/testes/# Auxiliar Estratificada.jl
+++ src/testes/# Auxiliar Estratificada.jl
@@ -13,7 +13,9 @@ nh = [7, 8, 7]
 nh1 = 7
 nh2 = 8
 nh3 = 7
-
+resultstring = ["Número de Unidades amostradas no estrato I", 
+"Número de Unidades amostradas no estrato II", 
+"Número de Unidades amostradas no estrato III"]
 function toRoman(n)
 
     roman_numerals = Dict(
@@ -22,13 +24,15 @@ function toRoman(n)
         400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"
     )
     result = ""
+
     for (value, numeral) in reverse(sort(collect(roman_numerals)))
         while n >= value
             n -= value
             result *= numeral
         end
     end
-    return "Número de Unidades Amostradas no Estrato $result"
+
+    return ["Número de Unidades Amostradas no Estrato $result" "Número potencial de Unidades do estrato $result"]
 end
 
 Informações_do_inventário = (
@@ -36,10 +40,8 @@ Informações_do_inventário = (
         Variáveis = [
             "Área da população (ha)", 
             "Número total potencial de Unidades da população", 
-            "Nível de significância (α)", 
-            "Número de Unidades amostradas no estrato I", 
-            "Número de Unidades amostradas no estrato II", 
-            "Número de Unidades amostradas no estrato III", 
+            "Nível de significância (α)",
+            
             "Número de estratos", "Número de Unidades totais", 
             "Número potencial de Unidades do estrato I", 
             "Número potencial de Unidades do estrato II", 
@@ -49,9 +51,9 @@ Informações_do_inventário = (
             Area, 
             N, 
             alpha, 
-            nh1, 
-            nh2, 
-            nh3, 
+            for i in 1:3
+                mm = nh[i]
+            end,
             length(unique(Estrato)), 
             length(Unidade), 
             (round((Area/(length(Unidade)))*nh1)*10), 
