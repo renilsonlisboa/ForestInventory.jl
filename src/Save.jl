af56f6e40c9382aa918b037c2c3882eb9041f37e
renilsonlisboa <renilsonlisboajunior@gmail.com>
2024-02-13 11:31:02 -0300

    modified:   src/Save.jl

--- src/Save.jl
+++ src/Save.jl
@@ -42,13 +42,13 @@ module Save
             names(Dados[3])), Anova_da_estratificação=(collect(eachcol(Dados[4])), 
             names(Dados[4])), Resultados=( collect(eachcol(Dados[5])), 
             names(Dados[5]))) #Export to Excel
-        elseif i in 6:8
+        elseif i === 6
             writetable("$(cleaned_path).xlsx", Dados=(collect(eachcol(Dados[1])), 
             names(Dados[1])), Primeira_ocasião=(collect(eachcol(Dados[2])), 
             names(Dados[2])), Segunda_ocasião=(collect(eachcol(Dados[3])), 
             names(Dados[3])), Crescimento_ou_mudança=(collect(eachcol(Dados[4])),   
             names(Dados[4])), overwrite = true) #Exportar para o Excel
-        elseif i in 9:9
+        elseif i === 9
             writetable(("$(cleaned_path).xlsx"), Dados=(collect(eachcol(Dados[1])), 
             names(Dados[1])), Informações_do_inventário=(collect(eachcol(Dados[2])), 
             names(Dados[2])), Primeira_ocasião=(collect(eachcol(Dados[3])), 
